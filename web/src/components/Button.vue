7d72899f22064f506004134495cae6792c7794e4
Emanuel Felipe <eemanuelleao@gmail.com>
2022-07-25 02:12:45 -0300

    add new components

---
+++ web/src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button
+        class="button"
+        :class="[props.color || 'primary']"
+    >
+        <icon class="icon" v-if="props.icon" :icon="props.icon"/>
+        {{props.title}}
+    </button>
+</template>
+<script setup lang="ts">
+import Icon, { IconType } from './Icon.vue';
+    type ButtonColors = 'primary' | 'secondary' | 'danger'
+
+    interface ButtonProps {
+        color?: ButtonColors;
+        icon?: IconType;
+        title?: string;
+    }
+
+    const props = defineProps<ButtonProps>();
+</script>
+<style scoped>
+    .icon {
+        margin-right: 0.5rem;
+    }
+</style>
