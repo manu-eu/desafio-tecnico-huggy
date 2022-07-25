ed1b475066ce52dac0fb5b2a01e3e2b4fece750f
Emanuel Felipe <eemanuelleao@gmail.com>
2022-07-25 02:13:41 -0300

    add base login page

--- web/src/pages/Login.vue
+++ web/src/pages/Login.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
+import VButton from '../components/Button.vue';
 
 </script>
 
 <template>
-Login
+    <div class="login">
+        <h1 class="headline-1">Login</h1>
+        <v-button title="Fazer login com a Huggy"/>
+    </div>
 </template>
 
 <style scoped>
+    .login {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        height: 100vh;
+    }
 
+    .login h1 ~ .button {
+        margin-top: 1rem;
+    }
 </style>
