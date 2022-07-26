b409954a12eba5481311d8d7e1e47810b8aac104
Emanuel Felipe <eemanuelleao@gmail.com>
2022-07-26 14:10:53 -0300

    🚧 add contact model factory

---
+++ api/database/factories/ContactFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'phone' => fake()->cellphoneNumber(),
+            'landline' => fake()->landline(),
+            'address' => fake()->address(),
+            'district' => 'Bairro '.fake()->word(),
+            'state' => fake()->state(),
+        ];
+    }
+}
