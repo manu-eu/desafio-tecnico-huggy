6ceb08de7b294c542073216d1c64c30e02302825
Emanuel Felipe <eemanuelleao@gmail.com>
2022-07-25 23:42:26 -0300

    💄 update BorderedBox style

--- web/src/components/BorderedBox.vue
+++ web/src/components/BorderedBox.vue
@@ -21,16 +21,15 @@
 <style scoped>
     .box {
         width: 100%;
-        min-height: calc(100vh - 4rem - 1.75rem);
+        /* min-height: calc(100vh - 4rem - 1.75rem); */
         background: #ffffff;
         border: 1px solid var(--mine-shaft-30);
         box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
         border-radius: 1rem;
-        padding: 1rem;
     }
 
     .box .box-header {
-        margin-bottom: 1rem;
+        padding: 1rem;
         display: flex;
         align-items: center;
         justify-content: space-between;
